610a5f80e1989333f8cf283ea4e4a9b23d8ded13
Charlie Cumber <charlie.cumber@softwire.com>
2021-02-26 10:03:05 +0000

    Project Set Up

---
+++ moot_app/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+from moot_app.flask_config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
