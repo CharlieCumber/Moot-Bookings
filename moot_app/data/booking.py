7bf34ab8de1ace511e85c933ddfb4157af381408
Charlie Cumber <charlie.cumber@softwire.com>
2021-02-28 01:32:30 +0000

    Form extension

--- moot_app/data/booking.py
+++ moot_app/data/booking.py
@@ -2,19 +2,54 @@ from moot_app.data import smartsheet
 from moot_app.data.countries import Countries
 
 class Booking:
-    def __init__(self, country="", participants=None):
+    def __init__(self, contact_first_name="", contact_last_name="", contact_position="", contact_email="", contact_phone="", country="", org_name="", org_email="", org_address="", org_website="", org_phone="", participants=None):
+        self.contact_first_name = contact_first_name
+        self.contact_last_name = contact_last_name
+        self.contact_position = contact_position
+        self.contact_email = contact_email
+        self.contact_phone = contact_phone
         self.country = country
+        self.org_name = org_name
+        self.org_email = org_email
+        self.org_address = org_address
+        self.org_website = org_website
+        self.org_phone = org_phone
         self.participants = participants
 
     @classmethod
     def fromDictionary(cls, dict=None):
         if dict == None:
             return cls()
-        return cls(dict['country'], dict['participants'])
+        return cls(
+            dict['contact_first_name'],
+            dict['contact_last_name'],
+            dict['contact_position'],
+            dict['contact_email'],
+            dict['contact_phone'],
+            dict['country'],
+            dict['org_name'],
+            dict['org_email'],
+            dict['org_address'],
+            dict['org_website'],
+            dict['org_phone'],
+            dict['participants'])
 
     @property
     def toSheetColumnDict(self):
-        return {'Country': self.country, 'Participants': self.participants}
+        return {
+            'Country': self.country,
+            'Participants': self.participants,
+            'Contact - First Name': self.contact_first_name,
+            'Contact - Last Name': self.contact_last_name,
+            'Contact - Position': self.contact_position,
+            'Contact - Email': self.contact_email,
+            'Contact - Phone': self.contact_phone,
+            'Organisation - Name': self.org_name,
+            'Organisation - Email': self.org_email,
+            'Organisation - Address': self.org_address,
+            'Organisation - Website': self.org_website,
+            'Organisation - Phone': self.org_phone
+            }
 
     @property
     def fee_category(self):
--- moot_app/forms/booking_form.py
+++ moot_app/forms/booking_form.py
@@ -1,8 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import Form, SelectField, IntegerField, validators
+from wtforms import Form, StringField, TextAreaField, SelectField, IntegerField, validators
 from wtforms.validators import DataRequired, NumberRange, Optional
 from moot_app.data.countries import Countries
 
 class BookingForm(FlaskForm):
+    contact_first_name = StringField('First name')
+    contact_last_name = StringField('Last name')
+    contact_position = StringField('Role')
+    contact_email = StringField('Email')
+    contact_phone = StringField('Phone number')
+    org_name = StringField('Organisation name')
+    org_email = StringField('Organisation email')
+    org_address = TextAreaField('Organisation address (including postal code)',)
+    org_website = StringField('Organisation website')
+    org_phone = StringField('Organisation phone number')
     country = SelectField(u'Country', choices=[("", "Select country")] + [(x['name'], x['name']) for x in Countries], validators=[DataRequired()])
     participants = IntegerField(u'Participants', validators=[DataRequired(), NumberRange(1, 500, "Enter a number from 1 to 500")])
